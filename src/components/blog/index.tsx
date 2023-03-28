581248bc08776a21092ff183edf95e8fc9c26a4d
meow <koptelovann@vogu35.ru>
2023-03-28 14:00:41 +0300

    Сделаны все блоки кроме "Получение оценки"

---
+++ src/components/blog/index.tsx
@@ -0,0 +1,33 @@
+const Blog = () => {
+    return (
+        <div class="media-47 page_section_63 section section_container-width_1200" id="">
+            <div class="media-47__inner section__content">
+              <div class="section__header section__header_align_center wysiwyg-string">
+                <h3>Смотрите наш&nbsp;видео-блог на <a href="https://www.youtube.com/@getauto2684/videos" target="_blank">YouTube</a></h3>
+              </div>
+
+              <div class="flexible-list flexible-list_columns_3 flexible-list_mobile-columns_1 media-47-list media-47__list">
+                <div class="media-47-item media-47-list__item flexible-list__item">
+                  <div class="media-47-item__video-container">
+                    <div class="media-47-item__video" parse-video-url="https://www.youtube.com/watch?v=aEhwsZTgSAw"></div>
+                  </div>
+                  <div class="media-47-item__text wysiwyg-content"><strong>Осмотр Hyundai Santa Fe 2022 года&nbsp;перед покупкой в Корее для&nbsp;нашего клиента из г. Москва.</strong></div>
+                </div>
+                <div class="media-47-item media-47-list__item flexible-list__item">
+                  <div class="media-47-item__video-container">
+                    <div class="media-47-item__video" parse-video-url="https://www.youtube.com/watch?v=CrFBRWX_0B8"></div>
+                  </div>
+                  <div class="media-47-item__text wysiwyg-content"><strong>Обзор купленного&nbsp;ROLLS-ROYCE CULLINAN&nbsp;600 л.с.</strong></div>
+                </div>
+                <div class="media-47-item media-47-list__item flexible-list__item">
+                  <div class="media-47-item__video-container">
+                    <div class="media-47-item__video" parse-video-url="https://www.youtube.com/watch?v=NE0YYBqzLpQ"></div>
+                  </div>
+                  <div class="media-47-item__text wysiwyg-content"><strong>Осмотр Kia Mohave 2022 года в Корее перед покупкой для клиента из Владивостока.</strong></div>
+                </div>
+              </div>
+            </div>
+          </div>
+    )
+}
+export { Blog }
---
+++ src/components/reviews/index.tsx
@@ -0,0 +1,52 @@
+const Reviews = () => {
+    return(
+    <><div className="page-section">
+            <div className="animated-section page_section_59 section section_container-width_800 text-11" id="">
+                <div className="section__background-image-container">
+                    <div className="section__background-image" style={{ backgroundImage: 'url(./images/005/143/693/5143693/1920xnoinc/yaponiya-avtoaukcion.jpg)' }} lazy-background=""></div>
+                </div>
+                <div className="section__background-mask"></div>
+
+                <div className="text-11__inner section__content">
+                    <div className="text-11__title wysiwyg-string">
+                        <div>Еще больше отзывов о нас</div>
+                    </div>
+                </div>
+            </div>
+        </div><div className="page-section">
+                <div className="page_section_60 section section_container-width_800 text-36" id="">
+                    <div className="section__background-image-container">
+                        <div className="section__background-image" style={{ backgroundImage: 'url(./images/005/143/693/5143693/1920xnoinc/yaponiya-avtoaukcion.jpg)' }} lazy-background=""></div>
+                    </div>
+                    <div className="section__background-mask"></div>
+
+                    <div className="text-36__inner section__content">
+                        <div className="text-36__title wysiwyg-string">
+                            <div>
+                                <a href="https://www.vl.ru/get-avto" target="_blank">VL.RU</a>&nbsp; /&nbsp;&nbsp;<a href="https://2gis.ru/nahodka/firm/70000001024036810/tab/reviews" target="_blank">2GIS</a>&nbsp;
+                                /&nbsp;&nbsp;<a href="https://yandex.ru/search/?text=get+auto+%D0%BD%D0%B0%D1%85%D0%BE%D0%B4%D0%BA%D0%B0&lr=75" target="_blank">YANDEX</a>&nbsp;
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><div className="page-section">
+                <div className="page_section_61 section section_container-width_1000 text-68" id="">
+                    <div className="section__background-image-container">
+                        <div className="section__background-image" style={{ backgroundImage: 'url(./images/005/143/693/5143693/1920xnoinc/yaponiya-avtoaukcion.jpg)'}} lazy-background=""></div>
+                    </div>
+                    <div className="section__background-mask"></div>
+
+                    <div className="text-68__inner section__content">
+                        <div className="text-68__title wysiwyg-string">
+                            <div>Еще больше отзывов о нас</div>
+                        </div>
+                        <div className="text-68__subtitle wysiwyg-string">
+                            <a href="https://www.vl.ru/get-avto" target="_blank">VL.RU</a> / <a href="https://2gis.ru/nahodka/firm/70000001024036810/tab/reviews" target="_blank">2GIS</a> /
+                            <a href="https://yandex.ru/search/?text=get+auto+%D0%BD%D0%B0%D1%85%D0%BE%D0%B4%D0%BA%D0%B0&lr=75" target="_blank">YANDEX</a>
+                        </div>
+                    </div>
+                </div>
+            </div></>
+    )
+}
+export { Reviews }
